870ce42e55ed4abe00629db82aa9628b50e0447b
ccrmota <ccrismota222@gmail.com>
2021-05-03 17:42:05 -0300

    Meu primeiro commit no GitHub

---
+++ app/src/main/java/br/cicero/nullable/FirstActivity.kt
@@ -0,0 +1,16 @@
+package br.cicero.nullable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+        var nome:String? ="Cicero"
+
+        nome = null
+
+    }
+}
